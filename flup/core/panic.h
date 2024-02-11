69235d6d574ad20bcf32969f4fb636b4fb65a89e
Foxie Flakey <foxieflakey@gmail.com>
2024-02-11 18:41:37 +0700

    Documenting flup/core/panic.h

--- flup/core/panic.h
+++ flup/core/panic.h
@@ -1,15 +1,41 @@
 #ifndef header_1704106484_33446cea_a0ec_438b_b03d_f7196b444150_panic_h
 #define header_1704106484_33446cea_a0ec_438b_b03d_f7196b444150_panic_h
 
+/**
+ * @file
+ * @brief Panic functions
+ *
+ * Contains function for panic even where code
+ * irrecoverably in erronous state.
+ */
+
 #include <stdarg.h>
 
 #include "flup/attributes.h"
 
+/**
+ * @brief Trigger a panic and never return
+ *
+ * Prints formatted panic message to log and/or
+ * stderr and then call abort().
+ *
+ * @param format Printf like format for panic message
+ * @param ... Arguments to format message
+ */
 FLUP_ATTR_PRINTF(1, 2)
 [[noreturn]]
 FLUP_PUBLIC
 void flup_panic(const char* format, ...);
 
+/**
+ * @brief Trigger a panic and never return
+ *
+ * Same as @ref flup_panic but uses va_list
+ *
+ * @param format Printf like format for panic message
+ * @param list va_list of arguments
+ */
+FLUP_ATTR_PRINTF(1, 0)
 [[noreturn]]
 FLUP_PUBLIC
 void flup_vpanic(const char* format, va_list list);
