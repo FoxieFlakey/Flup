9c6d250c60b63ead5edb66aabe5994734586cd12
Foxie Flakey <foxieflakey@gmail.com>
2024-02-11 17:33:08 +0700

    Document typecheck.h and split macro into more specific copies

--- flup/typecheck.h
+++ flup/typecheck.h
@@ -1,14 +1,60 @@
 #ifndef header_1706007321_a724a59c_4fab_4bf8_afd3_2f00c5df4a22_typecheck_h
 #define header_1706007321_a724a59c_4fab_4bf8_afd3_2f00c5df4a22_typecheck_h
 
-// Takes the desired type and returns pointer to "var"
+/**
+ * @file
+ * @brief Type check macros for macros
+ *
+ * Contain helper macros for checking
+ * types in a macro.
+ */
+
+/**
+ * @brief Check type for @a var's type.
+ * 
+ * Does not take expression only take
+ * variable name.
+ *
+ * @param var The variable to check
+ * @param type Type to check for as vararg
+ *
+ * @return @a var as @a type
+ */
 #define FLUP_CHECK_VAR_TYPE(var, ...) \
   (*((struct {__VA_ARGS__* __flup_check_ptr;}) \
      {&(var)}).__flup_check_ptr)
 
-#define FLUP_CHECK_VAR_FUNC_LIKE_TYPE(var, prefix, ...) \
-  (*((struct {prefix *__flup_check_ptr __VA_ARGS__;}) \
-     {&(var)}).__flup_check_ptr)
+/**
+ * @brief Check function type for @a func's type
+ *
+ * Does not take expression only take name.
+ *
+ * @param func Name/variable of function to check for type
+ * @param retType Return value of the function
+ * @param argTypes List of arguments types as vararg
+ *
+ * @return @a func as function with correct args
+ *   and return values
+ */
+#define FLUP_CHECK_VAR_FUNC_TYPE(func, retType, ...) \
+  (*((struct {retType (*__flup_check_ptr)(__VA_ARGS__);}) \
+     {&(func)}).__flup_check_ptr)
+
+/**
+ * @brief Check function type for @a block's type
+ *
+ * Does not take expression only take name.
+ *
+ * @param block Variable of block to check for type
+ * @param retType Return value of the block
+ * @param argTypes List of arguments types as vararg
+ *
+ * @return @a block as blocks with correct args
+ *   and return values
+ */
+#define FLUP_CHECK_VAR_BLOCK_TYPE(block, retType, ...) \
+  (*((struct {retType (^*__flup_check_ptr)(__VA_ARGS__);}) \
+     {&(block)}).__flup_check_ptr)
 
 #endif
 
--- flup/util/foreach.h
+++ flup/util/foreach.h
@@ -11,12 +11,11 @@
 // return false if nothing else to continue else 
 // true and Block_release won't be called
 #define flup_foreach(itemType, itemName, nextBlock, ...) \
-  for (itemType itemName __VA_OPT__(= FLUP_OPEN_PAREN) __VA_ARGS__ __VA_OPT__(FLUP_CLOSE_PAREN), *__flup_foreach_block = (void*) FLUP_CHECK_VAR_FUNC_LIKE_TYPE((nextBlock), bool FLUP_OPEN_PAREN ^, FLUP_CLOSE_PAREN (itemType*)); \
+  for (itemType itemName __VA_OPT__(= FLUP_OPEN_PAREN) __VA_ARGS__ __VA_OPT__(FLUP_CLOSE_PAREN), *__flup_foreach_block = (void*) FLUP_CHECK_VAR_FUNC_TYPE((nextBlock), bool, itemType*); \
        ((bool (^)(itemType*)) __flup_foreach_block)(&itemName);)
 
-#if FLUP_APPLE_BLOCKS_AVAILABLE
-# undef flup_foreach
-# define flup_foreach(...) static_assert("flup_foreach macro unavailable due Apple Blocks not enabled -w- (hint: -fblocks on Clang compiler)")
+#if !FLUP_APPLE_BLOCKS_AVAILABLE
+static_assert("flup_foreach.h unavailable due Apple Blocks not enabled -w- (hint: -fblocks on Clang compiler)");
 #endif
 
 #endif
