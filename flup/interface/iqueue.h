d3fe012031512e952fd545711186cceb459eb21c
Foxie Flakey <foxieflakey@gmail.com>
2024-03-18 17:48:33 +0700

    Document flup_iqueue

--- flup/interface/iqueue.h
+++ flup/interface/iqueue.h
@@ -5,21 +5,76 @@
 
 #include "flup/attributes.h"
 
-// Queue interface
+/**
+ * @file
+ * @copydoc flup_iqueue
+ */
 
+/**
+ * @brief The generic iqueue structure
+ * 
+ * This queue interface, dequeue/enqueue a bare void* pointer.
+ * @note Additional error code may exist depends on the implementation
+ */
 typedef struct flup_iqueue {
+  /// The pointer to operations
   struct flup_iqueue_ops* ops;
 } flup_iqueue;
 
+/**
+ * @brief Structure of operations supported by iqueue
+ */
 typedef struct flup_iqueue_ops {
+  /**
+   * @brief Get the length of queue
+   * 
+   * The length of items waiting in queue
+   * @note Do not use this to conditionally dequeue/enqueue, use
+   *   @ref dequeue or @ref enqueue with timeout at past instead.
+   *
+   * @param self The queue instance
+   *
+   * @return Length of items in queue
+   */
   unsigned int (*getLength)(flup_iqueue* self);
   
-  unsigned int (*try_enqueue)(flup_iqueue* self, void* ptr);
-  int (*enqueue)(flup_iqueue* self, void* ptr);
+  /**
+   * @brief Enqueue an item to queue
+   *
+   * Enqueues an item with timeout or no timeout ( @p abstimeout is NULL)
+   *
+   * @param self The queue instance
+   * @param item The item to be queued
+   * @param abstime The timeout in absolute time 
+   *
+   * @throws EINVAL Invalid arguments
+   * @throws ETIMEDOUT Time out reached (the queue is full for too long)
+   *
+   * @return 0 on sucess or -errno on error (see above)
+   */
+  int (*enqueue)(flup_iqueue* self, void* item, const struct timespec* abstimeout);
   
-  void* FLUP_NULLABLE (*try_dequeue)(flup_iqueue* self);
-  void* FLUP_NONNULL (*dequeue)(flup_iqueue* self, const struct timespec* abstimeout);
+  /**
+   * @brief Dequeues an item from queue
+   *
+   * Enqueues an item with timeout or no timeout ( @p abstimeout is NULL)
+   *
+   * @param self The queue instance
+   * @param[out] item The result of dequeued item
+   * @param abstime The timeout in absolute time 
+   *
+   * @throws EINVAL Invalid arguments
+   * @throws ETIMEDOUT Time out reached (the queue is empty for too long)
+   *
+   * @return 0 on sucess or -errno on error (see above)
+   */
+  int (*dequeue)(flup_iqueue* self, void** item, const struct timespec* abstimeout);
   
+  /**
+   * @brief Deallocates the iqueue instance
+   *
+   * @param self The queue itself
+   */
   void (*dealloc)(flup_iqueue* self);
 } flup_iqueue_ops;
 
