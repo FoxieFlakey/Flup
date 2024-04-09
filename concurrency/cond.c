0be3788741fad7c535e409791721855b5fe2d1ad
Foxie Flakey <foxieflakey@gmail.com>
2024-04-09 16:08:09 +0700

    Add condition

---
+++ concurrency/cond.c
@@ -0,0 +1,56 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "flup/concurrency/cond.h"
+#include "flup/attributes.h"
+#include "flup/bug.h"
+#include "flup/concurrency/mutex.h"
+
+FLUP_PUBLIC
+flup_cond* flup_cond_new() {
+  flup_cond* self = malloc(sizeof(*self));
+  if (!self)
+    return NULL;
+  
+  *self = (flup_cond) {};
+  if (pthread_cond_init(&self->cond, NULL) < 0) {
+    free(self);
+    return NULL;
+  }
+  self->initialized = true;
+  return self;
+}
+
+FLUP_PUBLIC
+void flup_cond_free(flup_cond* self) {
+  if (!self)
+    return;
+  
+  free(self);
+}
+
+FLUP_PUBLIC
+void flup_cond_wake_one(flup_cond* self) {
+  int ret = pthread_cond_signal(&self->cond);
+  BUG_ON(ret != 0);
+}
+
+FLUP_PUBLIC
+void flup_cond_wake_all(flup_cond* self) {
+  int ret = pthread_cond_broadcast(&self->cond);
+  BUG_ON(ret != 0);
+}
+
+FLUP_PUBLIC
+int flup_cond_wait(flup_cond* self, flup_mutex* mutex, const struct timespec* abstimeout) {
+  int ret;
+  if (abstimeout)
+    ret = pthread_cond_timedwait(&self->cond, &mutex->lock, abstimeout);
+  else
+    ret = pthread_cond_wait(&self->cond, &mutex->lock);
+  
+  BUG_ON(ret != 0);
+  return 0;
+}
+
---
+++ flup/concurrency/cond.h
@@ -0,0 +1,86 @@
+#ifndef UWU_D49C4473_4405_43B6_9BEB_F46D12F347FC_UWU
+#define UWU_D49C4473_4405_43B6_9BEB_F46D12F347FC_UWU
+
+#include <pthread.h>
+#include <time.h>
+
+#include "flup/attributes.h"
+#include "flup/concurrency/mutex.h"
+
+/**
+ * @file
+ * @copydoc flup_cond
+ */
+
+/**
+ * @brief Condition
+ *
+ * Its a structure which allows application to signal
+ * and wait for an event occur.
+ *
+ * The clock used as reference is CLOCK_MONOTONIC
+ */
+typedef struct flup_cond {
+  /// Is this structure initalized
+  bool initialized;
+  /// The pthread's cond structure
+  pthread_cond_t cond;
+} flup_cond;
+
+/**
+ * @brief Create new instance of @ref flup_cond
+ *
+ * @return New instance or NULL on error
+ */
+FLUP_PUBLIC
+flup_cond* flup_cond_new();
+
+/**
+ * @brief Free an instance of cond
+ *
+ * @param self The instance to be free'd
+ */
+FLUP_PUBLIC
+void flup_cond_free(flup_cond* self);
+
+/**
+ * @brief Wake one waiting thread
+ *
+ * @param self The condition which the function will wake one 
+ *   waiting thread from
+ */
+FLUP_PUBLIC
+void flup_cond_wake_one(flup_cond* self);
+
+/**
+ * @brief Wake all waiting threads
+ *
+ * @param self The condition which the function will wake waiting
+ *   threads from
+ */
+FLUP_PUBLIC
+void flup_cond_wake_all(flup_cond* self);
+
+/**
+ * @brief Wait on a condition
+ * 
+ * Put current thread to sleep until its woken up by @ref flup_cond_wake_one
+ * or @ref flup_cond_wake_all. Atomically unlock @p mutex before sleeping in
+ * respect to other threads and relocks on wake up
+ *
+ * @note Wake up may happen even when nobody waking up the thread
+ * @note Caller must already owned @p mutex when calling this function
+ *
+ * @param self The condition to wait on
+ * @param mutex The mutex to be unlock and lock
+ * @param abstimeout Absolute timeout while waiting, if NULL wait undefinitely
+ *
+ * @throws -ETIMEOUT Timeout reached, must not be returned if @p abstimeout
+ *   is equal to NULL
+ *
+ * @return 0 on success or -errno on error (see above)
+ */
+FLUP_PUBLIC
+int flup_cond_wait(flup_cond* self, flup_mutex* mutex, const struct timespec* abstimeout);
+
+#endif
