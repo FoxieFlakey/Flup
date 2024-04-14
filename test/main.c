8706e40e1f62bf6ecb69cc1822c8a17c5dc6f384
Foxie Flakey <foxieflakey@gmail.com>
2024-04-14 20:06:22 +0700

    Sample usage of flup_buffer, UwU

--- test/main.c
+++ test/main.c
@@ -1,10 +1,15 @@
-#include "flup/core/panic.h"
 #define _GNU_SOURCE
 #include <time.h>
 #include <assert.h>
 #include <sys/mman.h>
 #include <mimalloc.h>
 #include <stdbool.h>
+#include <locale.h>
+#include <ctype.h>
+#include <stddef.h>
+
+#include "flup/data_structs/buffer.h"
+#include "flup/thread/thread.h"
 
 #include <stdio.h>
 #include <unistd.h>
@@ -18,7 +23,91 @@ static void mimalloc_play();
 FLUP_PUBLIC
 int fluffedup_main(FLUP_UNUSED int argc, FLUP_UNUSED const char** argv) {
   flup_pr_info("Hello World!");
-  flup_panic("aaaa ouch");
+  
+  enum command {
+    COMMAND_SAY,
+    COMMAND_SHUTDOWN,
+    COMMAND_LOWERCASE,
+    COMMAND_UPPERCASE
+  };
+  
+  struct command_packet {
+    enum command cmd;
+    char* arg;
+  };
+  struct command_result {
+    int status;
+  };
+  
+  flup_buffer* commandBuffer = flup_buffer_new(sizeof(struct command_packet) * 32);
+  flup_buffer* completionBuffer = flup_buffer_new(sizeof(struct command_result) * 32);
+  
+  flup_thread* thread = flup_thread_new_with_block(^() {
+    struct command_packet packet;
+    struct command_result result;
+    
+    do {
+      flup_buffer_read_no_fail(commandBuffer, &packet, sizeof(packet));
+      
+      int ret = 0;
+      switch (packet.cmd) {
+        case COMMAND_SAY:
+          printf("[String Processor] Received command to say the string\n");
+          printf("[String Processor] Saying '%s'\n", packet.arg);
+          break;
+        case COMMAND_LOWERCASE:
+          printf("[String Processor] Received command to lowercase a string\n");
+          for (size_t i = 0; packet.arg[i] != '\0'; i++)
+            packet.arg[i] = (char) tolower(packet.arg[i]);
+          printf("[String Processor] Processed with succes!\n");
+          ret = 0;
+          break;
+        case COMMAND_UPPERCASE:
+          printf("[String Processor] Received command to uppercase a string\n");
+          for (size_t i = 0; packet.arg[i] != '\0'; i++)
+            packet.arg[i] = (char) toupper(packet.arg[i]);
+          printf("[String Processor] Processed with succes!\n");
+          ret = 0;
+          break;
+        case COMMAND_SHUTDOWN:
+          printf("[String Processor] Received shutdown command, shutting down bye!\n");
+          break;
+      }
+      
+      result.status = ret;
+      flup_buffer_write_no_fail(completionBuffer, &result, sizeof(result));
+    } while (packet.cmd != COMMAND_SHUTDOWN);
+  });
+  
+  struct command_packet cmd;
+  struct command_result result;
+  
+  char str[] = "Hello World!";
+  
+  cmd = (struct command_packet) {COMMAND_LOWERCASE, str};
+  flup_buffer_write_no_fail(commandBuffer, &cmd, sizeof(cmd));
+  printf("[Parent] Sent command to lower case a string\n");
+  
+  flup_buffer_read_no_fail(completionBuffer, &result, sizeof(result));
+  printf("[Parent] Lower cased string, result is %s\n", str);
+  
+  cmd = (struct command_packet) {COMMAND_UPPERCASE, str};
+  flup_buffer_write_no_fail(commandBuffer, &cmd, sizeof(cmd));
+  printf("[Parent] Sent command to upper case a string\n");
+  
+  flup_buffer_read_no_fail(completionBuffer, &result, sizeof(result));
+  printf("[Parent] Upper cased string, result is %s\n", str);
+  
+  cmd = (struct command_packet) {COMMAND_SHUTDOWN, NULL};
+  flup_buffer_write_no_fail(commandBuffer, &cmd, sizeof(cmd));
+  printf("[Parent] Sent shutdown command\n");
+  
+  flup_buffer_read_no_fail(completionBuffer, &result, sizeof(result));
+  printf("[Parent] Shutdown successful\n");
+  
+  flup_thread_free(thread);
+  flup_buffer_free(commandBuffer);
+  flup_buffer_free(completionBuffer);
   // mimalloc_play();
   return 0;
 }
