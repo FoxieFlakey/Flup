2f8b3908be66998f972d2df6486d545baf2e10ce
Foxie Flakey <foxieflakey@gmail.com>
2024-05-11 18:15:27 +0700

    Add general hash function typedef

---
+++ flup/hashing/hash_func.h
@@ -0,0 +1,20 @@
+#ifndef UWU_E7D539E6_51C3_48CF_B780_6FDD14CDB5D4_UWU
+#define UWU_E7D539E6_51C3_48CF_B780_6FDD14CDB5D4_UWU
+
+#include <stdint.h>
+#include <stddef.h>
+
+/**
+ * @file
+ * @brief Containing typedefs for hash functions
+ */
+
+/**
+ * @brief A general purpose data to uintptr_t hash function
+ *
+ * @param data The input data
+ * @param size Size of the input data
+ */
+typedef uintptr_t (*flup_hash_func)(const void* data, size_t size);
+
+#endif
