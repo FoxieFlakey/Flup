3ba56ee3af064a668631d5995231b4b585f377b0
Foxie Flakey <foxieflakey@gmail.com>
2024-05-11 20:09:04 +0700

    Change to use flup_hash<X> types instead

--- flup/hashing/noncrypto/xxhash.h
+++ flup/hashing/noncrypto/xxhash.h
@@ -1,10 +1,10 @@
 #ifndef UWU_F870BEE6_CD1A_4586_AEFF_D147F7129A15_UWU
 #define UWU_F870BEE6_CD1A_4586_AEFF_D147F7129A15_UWU
 
-#include <stdint.h>
 #include <stddef.h>
 
 #include "flup/attributes.h"
+#include "flup/hashing/hash_func.h"
 
 /**
  * @file
@@ -20,7 +20,7 @@
  * @return The hashed data
  */
 FLUP_PUBLIC
-uint64_t flup_xxhash3_hash_64bits(const void* data, size_t size);
+flup_hash64 flup_xxhash3_hash_64bits(const void* data, size_t size);
 
 /**
  * @brief Hash using XXH3 algorithmn (128-bit)
@@ -31,6 +31,6 @@ uint64_t flup_xxhash3_hash_64bits(const void* data, size_t size);
  * @return The hashed data
  */
 FLUP_PUBLIC
-_BitInt(128) flup_xxhash3_hash_128bits(const void* data, size_t size);
+flup_hash128 flup_xxhash3_hash_128bits(const void* data, size_t size);
 
 #endif
